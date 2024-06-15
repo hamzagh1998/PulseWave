58ac010f305ea824a23ce0a91f6451c45e039e9f
hamzagh1998 <hamzaghenimi98@gmail.com>
2024-06-15 22:21:47 +0100

    fix

--- src/pages/auth/_root-layout.tsx
+++ src/pages/auth/_root-layout.tsx
@@ -4,11 +4,11 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
   const theme = useCurrentTheme();
   return (
     <div className="p-4 flex justify-center items-center flex-col h-screen">
-      <div className="lg:grid lg:grid-cols-12 gap-20">
-        <div className="lg:col-span-6 hidden lg:block">
+      <div className="lg:grid lg:grid-cols-12 gap-20 h-screen">
+        <div className="hidden lg:flex lg:col-span-6 justify-center items-center flex-col">
           <div className="flex justify-center items-center gap-6">
             <img
-              className="w-40 h-4w-40 mb-4"
+              className="w-40 h-40 mb-4"
               src={
                 theme === "dark" ? "wave-logo-dark.png" : "wave-logo-light.png"
               }
@@ -16,13 +16,13 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
             />
             <p className="text-6xl">WavePulse</p>
           </div>
-          <div className="flex justify-start items-center">
-            <p className="text-xl text-muted-foreground">
-              Connecting You with with friends and the world around you!
+          <div className="flex justify-center items-center">
+            <p className="text-xl text-muted-foreground text-center">
+              Connecting You with friends and the world around you!
             </p>
           </div>
         </div>
-        <div className="w-full flex justify-enitems-end items-center flex-col lg:col-span-6">
+        <div className="w-full flex justify-center items-center flex-col lg:col-span-6">
           <div className="w-full flex justify-center items-center gap-4">
             <img
               className="w-20 h-20 mb-4 sm:block"
